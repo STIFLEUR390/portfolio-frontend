6e40149476ac992aa5af751ff1d3f4ebbea89228
DEV MASTER <53788141+STIFLEUR390@users.noreply.github.com>
2025-07-05 13:08:05 +0100

    Ajout de la configuration Tailwind CSS et des dépendances associées, création de fichiers de style et d'utilitaires pour l'application Nuxt. Mise à jour de la configuration Nuxt pour intégrer les nouveaux modules et styles.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,30 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/image', '@nuxt/fonts', '@nuxt/eslint']
+  modules: [
+    '@nuxt/image',
+    '@nuxt/fonts',
+    '@nuxt/eslint',
+    'shadcn-nuxt',
+  ],
+  css: ['~/assets/css/tailwind.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  }
 })
